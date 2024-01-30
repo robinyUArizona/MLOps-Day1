586d861cdf5bb92f0a007add5c6bbf6d3273c44d
robinyUArizona <robiny@arizona.edu>
2024-01-30 14:16:08 +0545

    added init inside exception

--- template.py
+++ template.py
@@ -17,6 +17,7 @@ list_of_files = [
     "src/utils/utils.py",
     "src/logger/logging.py",
     "src/exception/exception.py",
+    "src/exception/__init__.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
