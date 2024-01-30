f974878f9aa0ba7d9a614da62e5b143df545e5a8
robinyUArizona <robiny@arizona.edu>
2024-01-30 13:26:35 +0545

    Updated template.py

--- template.py
+++ template.py
@@ -1,6 +1,5 @@
 import os # to interact with OS
 from pathlib import Path
-import logging
 
 list_of_files = [
 
@@ -36,7 +35,6 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for file: {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
